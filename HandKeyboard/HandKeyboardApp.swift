3e31c6d70292016b64b10f5d48ae7b7e97764cd5
C171017 <c171017xjwl@gmail.com>
2025-04-12 21:58:17 -0400

    Initial Commit

---
+++ HandKeyboard/HandKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandKeyboardApp.swift
+//  HandKeyboard
+//
+//  Created by C171017 on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HandKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HandKeyboardTests/HandKeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  HandKeyboardTests.swift
+//  HandKeyboardTests
+//
+//  Created by C171017 on 4/12/25.
+//
+
+import Testing
+@testable import HandKeyboard
+
+struct HandKeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
